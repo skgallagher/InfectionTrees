f29133618e2b206cf12cc1ade876a457e191937f
Shannon <skgallagher19@gmail.com>
2021-06-23 10:56:25 -0400

    added naive bootstrap

--- revised_paper_results/md-tb/analyze-data-big-table.R
+++ revised_paper_results/md-tb/analyze-data-big-table.R
@@ -1,6 +1,7 @@
 ## SKG
 ## August 14, 2020
 ## Combining the tables together from three model types
+## Updated June 22, 2021
 
 devtools::load_all()
 library(knitr)
@@ -12,7 +13,8 @@ library(kableExtra)
 ## loglike table BASE MODEL
 files <- list.files()
 fn_rds <- grep("RDS", files, value = TRUE)
-fn <- grep("base", fn_rds, value = TRUE)
+fn <- fn_rds[1]
+
 data_list_base <- readRDS(fn)
 
 loglike_df_base <- data_list_base$loglike_df
@@ -29,8 +31,8 @@ loglike_df_base$type <- "Base"
 
 files_o <- list.files()
 fn_rds_o <- grep("RDS", files_o, value = TRUE)
-fn_rds2_o <- fn_rds_o[grep("outsider", fn_rds_o)]
-fn_o <- fn_rds2_o[5] ## 8-11-22 is date this should be/  I used last
+fn_rds2_o <- fn_rds_o[grep("mot", fn_rds_o)]
+fn_o <- fn_rds2_o[1] 
 
 data_list_o <- readRDS(fn_o)
 
@@ -43,32 +45,37 @@ loglike_df_o$vars <- c("Null",
                        "Smear/HIV/rel. time",
                        "Smear/HIV/rel. time/race")
 loglike_df_o$type <- "Multiple outside"
-
-
+#########################################################################
+## TABLE 5 - loglike for models
+#####################################################
 combined_loglike_df <- dplyr::bind_rows(loglike_df_base,
                                         loglike_df_o) %>%
     select(type, everything())
 
 combined_loglike_df %>%
-    select(type, Model, vars, n_params, loglike, aic) %>%
+    select(type, model, vars, n_params, loglike, aic) %>%
     kable(format = "latex", digits = 2,
           caption = "Reported log likelihood for the Maryland TB data for the five different models described in Eq. \\eqref{eq:data-models} for both the base and multiple outside transmissions model types.",
           label = "data-models",
           col.names = c("Type", "Model #", "Covariates", "# parameters", "Log like", "AIC"),
           booktabs = TRUE,
           linesep = "") %>%
-    kable_styling(latex_options = "striped", position = "center") %>%
-    row_spec(c(4,9), background = "yellow") %>%
+    kable_styling(latex_options = "striped", position = "center",
+                  stripe_index = c(1, 3, 5, 7)) %>%
+    row_spec(c(4), background = "yellow") %>%
+    row_spec(c(9), background = "yellow") %>%
     row_spec(c(5), extra_latex_after = "\\midrule") 
 
 
+#########################################################
 ###############################################################
+## TABLE 6 - best model coefficients
 ## BEST MODELS
 ## smear/HIV/rel time
 
 
 ## BASE
-best_mod_base2 <- (data_list_base$output_list[[4]])$est_pars
+best_mod_base2 <- data_list_base$beta_list[[4]][,c(1, 4)]
 coeff_names <- c("Intercept", "Smear pos.",
                  "HIV neg.: HIV pos.",
                  "HIV unk.: HIV pos.",
@@ -79,7 +86,7 @@ best_mod_base <- data.frame(Type = "Base",
                             best_mod_base2)
 
 ## OUTSIDER
-best_mod_o2 <- (data_list_o$output_list[[4]])$est_pars
+best_mod_o2 <- data_list_o$beta_list[[4]][,c(1,4)]
 coeff_names <- c("Intercept", "Smear pos.",
                  "HIV neg.: HIV pos.",
                  "HIV unk.: HIV pos.",
@@ -90,7 +97,7 @@ best_mod_o <- data.frame(Type = "Multiple outside",
                          best_mod_o2)
 
 
-##  NAIVE MODEL
+## NAIVE MODEL
 best_mod_naive2 <- data.frame(readRDS("naive-tab.RDS"))
 coeff_names <- c("Intercept", "Smear pos.",
                  "HIV neg.: HIV pos.",
@@ -99,13 +106,21 @@ coeff_names <- c("Intercept", "Smear pos.",
 rownames(best_mod_naive2) <- NULL
 best_mod_naive <- data.frame(Type = "Naive",
                             Coeff = coeff_names,
-                         best_mod_naive2)
+                            best_mod_naive2[, c(1, 4)]) %>%
+    rename("Est." = Mean)
 
 ########
 ## Putting it all together
 combined_best_mod <- rbind(best_mod_base,
                            best_mod_o,
-                           best_mod_naive)
+                           best_mod_naive) %>%
+    mutate(or = exp(`Est.`),
+           lower = exp(`Est.`- 1.96 * SE),
+           upper = exp(`Est.` + 1.96 * SE)) %>%
+    mutate(pretty_or = paste0(round(or, 2), " [",
+                              round(lower,2), ", ",
+                              round(upper, 2), "]"))
+           
 
 
 
@@ -115,13 +130,18 @@ combined_best_mod <- rbind(best_mod_base,
 
 
 combined_best_mod %>%
+    select(-c(lower, upper, or)) %>%
     kable(format = "latex", booktabs = TRUE,
           col.names = c("Type", "Coeff.",
-                        "Mean Est.", "Lower", "Upper", "SE"),
-          caption = "Coefficient estimates for the best selected model.  The columns `Lower' and `Upper' comprise the lower and upper bound estimates, respectively,  of the 95\\% CI, which come from likelihood profiling.  The highlighted rows are significant coefficients at the $\\alpha = .05$-level.  We also report the standard error of the estimate (SE).",
+                        "$\\hat{\\beta}$",  "$\\widehat{SE}(\\hat{\\beta})$",
+                        "Odds Ratio and 95\\% CI"),
+          caption = "Coefficient estimates for the best selected model.  We report the mean estimate and standard error of the $\\beta$ values in addition to the odds ratio and 95\\% CI.",
           label = "best-model-ests",
           digits = 2,
-          linesep = "") %>%
-    kable_styling(latex_options = "striped", position = "center") %>%
-    row_spec(c(3, 5, 8, 9, 10, 13), background = "yellow") %>%
+          linesep = "",
+          escape = FALSE,
+          align = c("l", "l", "c", "c", "c")) %>%
+    kable_styling(latex_options = "striped", position = "center",
+                  stripe_index = c(1,3, 7, 9, 11, 13)) %>%
+    row_spec(c(5, 10, 15), background = "yellow") %>%
     row_spec(c(5, 10), extra_latex_after = "\\midrule") 
---
+++ revised_paper_results/md-tb/data-naive-boot.R
@@ -0,0 +1,129 @@
+## SKG
+## Testing out model where transmission tree is determined by infection order
+## Updated June 23, 2021 to add a bootstrap SE
+
+## Load libraries and data
+devtools::load_all()
+library(tidyverse)
+data(tb_clean)
+
+my_seed <- 6172021
+set.seed(my_seed)
+
+t0 <- proc.time()[3]
+
+## Format the data for all the variables
+
+## I'm filling in the Unknown with a negative smear
+## Should later check to see if results are changed with positive
+tb_sub <- tb_clean %>%
+        dplyr::mutate(smear = ifelse(spsmear == "Positive",
+                                     1, 0),
+                      cluster_id = group,
+                      hiv_f = ifelse(hivstatus == "Negative", "neg",
+                                     ifelse(hivstatus == "Positive", "pos",
+                                            "unk"))) %>%
+        dplyr::mutate(hiv_neg_pos = ifelse(hiv_f == "neg", 1, 0),
+               hiv_unk_pos = ifelse(hiv_f == "unk", 1, 0)) %>%
+        dplyr::group_by(cluster_id) %>%
+        dplyr::mutate(rel_time = as.numeric(rel_time / 365)) %>%
+        dplyr::mutate(cluster_size = dplyr::n()) %>%
+        dplyr::ungroup() %>%
+    mutate(race_f = fct_collapse(race,
+                                 white = "White",
+                                 black = "Black or African American",
+                                 asian = "Asian")) %>%
+    mutate(race_asian_white = ifelse(race_f == "asian", 1, 0),
+           race_black_white = ifelse(race_f == "black", 1, 0)) %>%
+    select(cluster_id, smear,
+           hiv_neg_pos,
+           hiv_unk_pos,
+           rel_time,
+           race_asian_white,
+           race_black_white,
+           cluster_size)
+tb_orig <- tb_sub
+
+
+
+
+naive_loglike <- function(inf_params, dt,
+                          covariate_names, cov_mat,
+                          return_neg = TRUE){
+
+    dt <- dt[, prob_inf :=
+                   1 / (1 + exp(-(cov_mat %*% inf_params)))]
+    ## Trying out data.table
+    loglike_df <- dt[,
+                     .(loglike = naive_cluster_loglike(prob_inf, rel_time)),
+                            by = .(cluster_id)]
+    loglike <- sum(loglike_df$loglike)
+    if(return_neg) loglike <- -loglike
+    return(loglike)
+    
+
+
+}
+
+# cluster contains column datecoll which is the date of collection
+naive_cluster_loglike <- function(prob_inf, datecoll){
+    last_ind <- which.max(datecoll)
+    probs <- prob_inf[-last_ind]
+    loglike <- sum(log(probs)) + sum(log(1 - prob_inf))
+    return(loglike)
+
+}
+
+
+
+######################3
+## ORIGINAL
+set.seed(7272021)
+beta1_vec <- numeric(10)
+B <- 100
+
+
+beta_mat <- matrix(0, nrow = B, ncol = 5)
+cov_mat <- matrix(0, nrow = B, ncol = 5)
+for(jj in 1:B){
+
+    ## boot strap orig data
+    tb_sub <- bootstrap_clusters(tb_orig)
+    
+   
+
+    covariate_names <- c("smear", "hiv_neg_pos", "hiv_unk_pos", "rel_time")
+    cov_mat <- covariate_df_to_mat(tb_sub,
+                                   cov_names = covariate_names)
+    init_params <- rep(0, length(covariate_names) + 1)
+    bds <- rep(-5, length(covariate_names) + 1)
+    t1 <- proc.time()[3]
+    best_params <- optim(par = init_params,
+                         fn = naive_loglike,
+                         dt = data.table::as.data.table(tb_sub),
+                         return_neg = TRUE,
+                         cov_mat = cov_mat,
+                         covariate_names = covariate_names,
+                         method = "L-BFGS-B",
+                         lower = bds,
+                         upper = -bds,
+                         hessian = TRUE
+                         )
+    t2 <- proc.time()[3] - t1
+    print(paste("Optimization time:", round( t2 / 60, 3),
+                "min"))
+    se <- sqrt(diag(solve(best_params$hessian)))
+    beta_mat[jj,] <- best_params$par
+    lower <- best_params$par - 1.96 * se
+    upper <- best_params$par + 1.96 * se
+    cov_mat[jj,] <- ifelse(best_params$par >= lower &
+                      best_params$par <= upper, 1, 0)
+ 
+    print(-best_params$value)
+    print(best_params$par, digits = 2)
+}
+
+se_boot <- apply(beta_mat, 2, sd)
+
+saveRDS(list(beta_mat = beta_mat,
+           se_boot = se_boot), "naive-tab-boot.RDS")
